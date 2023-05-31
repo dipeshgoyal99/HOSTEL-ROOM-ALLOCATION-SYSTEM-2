1bf0db887e8b01449217b304fa1dab27aa2be799
dipesh <dr.dipeshgoyal265@gmail.com>
2023-05-31 09:46:16 +0530

    nsp project

---
+++ student/about.php
@@ -0,0 +1,107 @@
+
+<html>
+
+<head>
+  <title>About us</title>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+    body,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      font-family: "Raleway", Arial, Helvetica, sans-serif
+    }
+  </style>
+
+<body class="w3-light-grey">
+  </head>
+
+  <body>
+    <div class="topnav" id="myTopnav">
+      <a href="dashboard.php" class="active">Home</a>
+
+      <a href="dashboard.php">Back</a>
+
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+      </a>
+    </div>
+
+
+    <div class="w3-row-padding" id="about">
+      <div class="w3-col l4 12">
+        <h3>About</h3>
+        <h4> Our hostel is one of the best hostel with all facilities.There are different hostels for rboth men and
+          women iin different area for boys there are 8 hostels with name starting from GS-5 to 8 and for girls GS- 1 to
+          4. In every hosTtel almost there are more than 100 hostels which includes 3 blocks and a shopping store,
+          canteen and mess.For every year the hostel fee varies for student based on the room he/she taken i,e 30k to
+          120k and we are provided with 24 hour current facility.
+
+          There are PG available right outside the campuss and the charge varies from 3000 to 5000 per month and also
+          there is acvvailable with continous auto  facility to get to campss very easily and also campuss shuttle which
+          charge only 10 rupees for a ride anywhere in campuss.
+
+          The quality of the food is really good and whenever you go you wuill alsways get hot food in you plate and
+          there is no limit to how much you can eat except for the sweet dish and it costs around 26k for year.
+          The class we get are very spacious and only 60 students are the max we get in a class and sometime we get to
+          have combines class then we will be having a very large room where we will bw provided with 4 techers to teach
+          us and we use wifi which uses optical fibere technology and speed is around 120kbs to 3mbps based on the time
+          we connect to it.</h4>
+        <p>We accept: <i class="fa fa-credit-card w3-large"></i> <i class="fa fa-cc-mastercard w3-large"></i> 
+          <iclass="fa fa-cc-amex w3-large"></iclass=> <i class="fa fa-cc-cc-visa w3-large"></i><i
+            class="fa fa-cc-paypal w3-large"></i></p>
+      </div>
+      <div class="w3-col l8 12">
+        <!-- Image of location/map -->
+        <img src="../m/about12.jpg" class="w3-image w3-greyscale" style="height: 50%; width:100%;">
+      </div>
+    </div><br><br>
+
+
+
+    <!-- Team Container -->
+    <div class="w3-container w3-padding-64 w3-center" id="team">
+      <h2>OUR TEAM</h2>
+      <h3><em>Meet our team :</em></h3>
+
+      <div class="w3-row"><br>
+
+        <div class="w3-quarter">
+          <img src="../photos/fr2.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+          <h3>Garvesh Jangid</h3>
+          <p>Chancellor</p>
+        </div>
+
+        <div class="w3-quarter">
+          <img src="../photos/fr1.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+          <h3>Arpit Mittal</h3>
+          <p>Pro Chancellor</p>
+        </div>
+
+        <div class="w3-quarter">
+          <img src="../photos/fr4.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+          <h3>Ayush Agrawal</h3>
+          <p>vice Chancellor</p>
+        </div>
+
+        <div class="w3-quarter">
+          <img src="../photos/fr7.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+          <h3>Dipesh Goyal</h3>
+          <p>Founder Chairman</p>
+        </div>
+
+      </div>
+    </div>
+
+
+
+
+  </body>
+
+</html>
+
